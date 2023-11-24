b038917d3247504c4c51d2c469865da81e7bcd33
bimoalfarrabi <bimoalfarrabi24@gmail.com>
2023-11-24 10:16:40 +0700

    Implement Material Design 3 Search Bar

--- app/src/main/java/com/viasco/submission/compose/androver/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/viasco/submission/compose/androver/ui/screen/HomeScreen.kt
@@ -6,8 +6,17 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -56,6 +65,7 @@ fun HomeScreen(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeContent(
     query: String,
@@ -63,12 +73,32 @@ fun HomeContent(
     listAndroVer: List<AndroVer>,
     onFavoriteIconClick: (id: Int, newState: Boolean) -> Unit,
     navigateToDetail: (Int) -> Unit,
+    modifier: Modifier = Modifier
 ) {
     Column{
-        SearchBar(
+        androidx.compose.material3.SearchBar(
             query = query,
-            onQueryChange = onQueryChange
-        )
+            onQueryChange = onQueryChange,
+            onSearch = {},
+            active = false,
+            onActiveChange = {},
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            },
+            placeholder = {
+                Text(stringResource(R.string.search_androver))
+            },
+            shape = MaterialTheme.shapes.large,
+            modifier = modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+                .heightIn(min = 48.dp)
+        ) {
+        }
         if (listAndroVer.isNotEmpty()) {
             ListAndroVer(
                 listAndroVer = listAndroVer,
